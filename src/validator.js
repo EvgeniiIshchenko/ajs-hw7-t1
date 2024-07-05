9d0ebd1add4e92b5d4135e0198f6fbef1627337a
Ishchenko Evgenii <Evgenii.Ishchenko@yandex.ru>
2024-07-05 07:33:20 +0300

    second commit

--- src/validator.js
+++ src/validator.js
@@ -3,8 +3,3 @@ export class Validator {
         return /^(?!.*[\d]{3})[^0-9_-][a-zA-Z0-9-_]+[^0-9_-]$/.test(name);
     }
 }
-
-const nick = new Validator();
-
-console.log(nick.validateUsername('qweqw32e-rw-e-q_q1we'));
-
